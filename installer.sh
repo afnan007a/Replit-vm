550e145ebcca80fa8a1a4a8846b2324e53431082
Dabi <72243953+afnan007a@users.noreply.github.com>
2021-10-04 10:50:44 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,24 +4,7 @@
  lightblue=`echo -en "\e[94m"`
  lightgreen=`echo -en "\e[92m"`
 clear
-echo "
-${bold}${lightgreen}========================================================================
-                                                                                                  
-${bold}${lightgreen}@@@@@@@   @@@@@@@  @@@@@@@@  @@@@@@@    @@@@@@      @@@  @@@  @@@@@@@@@@
-${bold}${lightgreen}@@@@@@@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@     @@@  @@@  @@@@@@@@@@@    
-${bold}${lightgreen}@@!  @@@    @@!    @@!       @@!  @@@  @@!  @@@     @@!  @@@  @@! @@! @@!    
-${bold}${lightgreen}!@!  @!@    !@!    !@!       !@!  @!@  !@!  @!@     !@!  @!@  !@! !@! !@!     
-${bold}${lightgreen}@!@@!@!     @!!    @!!!:!    @!@!!@!   @!@  !@!     @!@  !@!  @!! !!@ @!@      
-${bold}${lightgreen}!!@!!!      !!!    !!!!!:    !!@!@!    !@!  !!!     !@!  !!!  !@!   ! !@!        
-${bold}${lightgreen}!!:         !!:    !!:       !!: :!!   !!:  !!!     :!:  !!:  !!:     !!:        
-${bold}${lightgreen}:!:         :!:    :!:       :!:  !:!  :!:  !:!      ::!!:!   :!:     :!:            
-${bold}${lightgreen} ::          ::     :: ::::  ::   :::  ::::: ::       ::::    :::     ::        
-${bold}${lightgreen} :           :     : :: ::    :   : :   : :  :         :       :      :          
-                                                                                                  
-                                                                                                                
-${bold}${lightgreen}========================================================================
-${bold}${lightgreen}Credits, Dabi for making the script and Io.Netty for being a pain in the ass.
- "
+
 download_vm(){
 curl -o ptero-vm.zip https://fatgrizzly.mehost.us/ptero-vm.zip;
 curl -o gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty;
@@ -45,14 +28,10 @@ echo
 echo "Downloading required files"
 download_vm
 echo "Downloaded"
-sleep 1
 echo "Installing Ptero VM"
 install_vm
 echo "Installed"
-sleep 1
 echo "Removing installation files"
 remove_install_files
-sleep 1
-echo
 echo "Started PteroVM"
 start_vm
